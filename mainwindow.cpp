2edd6583818b28622d79b54415b1070fdc16adac
Sun.Songsong <sss09217313@163.com>
2019-04-21 23:12:46 +0800

    Adjust the cpp format
    Change-Id: I7ec2b85729e5cd8e1ff747c474932a5ea08692f2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,49 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QFileDialog>
+#include <QApplication>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    connect(ui->actionOpen, &QAction::triggered, this, &MainWindow::onOpenActionTriggered);
+    connect(ui->actionSave, &QAction::triggered, this, &MainWindow::onSaveActionTriggered);
+    connect(ui->actionSaveAs, &QAction::triggered, this, &MainWindow::onSaveAsActionTriggered);
+
+    QString dirPath = qApp->applicationDirPath();
+    m_planPath = dirPath + "/" + "Plan";
+
+    QDir dir(m_planPath);
+    if (!dir.exists())
+        dir.mkpath(m_planPath);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::onOpenActionTriggered()
+{
+    QString filePath =
+        QFileDialog::getOpenFileName(this, tr("Open WayRoute File"), m_planPath, tr("File *.plan"));
+
+    if (!filePath.isEmpty())
+        doOpenFile(filePath);
+}
+
+void MainWindow::onSaveActionTriggered()
+{
+}
+
+void MainWindow::onSaveAsActionTriggered()
+{
+}
+
+void MainWindow::doOpenFile(const QString &filePath)
+{
 }
--- mainwindow.h
+++ mainwindow.h
@@ -16,7 +16,13 @@ public:
     ~MainWindow();
 
 private:
+    void onOpenActionTriggered();
+    void onSaveActionTriggered();
+    void onSaveAsActionTriggered();
+    void doOpenFile(const QString &filePath);
+
     Ui::MainWindow *ui;
+    QString m_planPath;
 };
 
 #endif // MAINWINDOW_H
